e1b0c38ff7d36515dc7d8141fde1c0ec599a893d
Scott Silvi <mergebandit@gmail.com>
2018-10-24 16:27:47 -0400

    Adding linting

--- index.js
+++ index.js
@@ -1,13 +1,32 @@
 import { createPlugin } from 'docz-core'
 
-export const myPlugin = () => createPlugin({
-  setConfig: (config) => /* ... */,
-  modifyBundlerConfig: (config, dev) => /* ... */,
-  modifyBabelRc: (babelrc) => /* ... */,
-  onCreateApp: (app) => /* ... */,
-  onServerListening: (server) => /* ... */,
-  onPreBuild: () => /* ... */,
-  onPostBuild: (config) => /* ... */,
-  onPreRender: (config) => /* ... */,
-  onPostRender: () => /* ... */,
-})
+export const myPlugin = () =>
+  createPlugin({
+    setConfig: config => {
+      /* ... */
+    },
+    modifyBundlerConfig: (config, dev) => {
+      /* ... */
+    },
+    modifyBabelRc: babelrc => {
+      /* ... */
+    },
+    onCreateApp: app => {
+      /* ... */
+    },
+    onServerListening: server => {
+      /* ... */
+    },
+    onPreBuild: () => {
+      /* ... */
+    },
+    onPostBuild: config => {
+      /* ... */
+    },
+    onPreRender: config => {
+      /* ... */
+    },
+    onPostRender: () => {
+      /* ... */
+    }
+  })
