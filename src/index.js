9af8d0a78624b1994b02ed591582b2d0acb406ee
Travis Fischer <fisch0920@gmail.com>
2018-10-24 22:47:06 -0400

    adding WIP notes

--- src/index.js
+++ src/index.js
@@ -13,6 +13,13 @@ export const storybook = (opts = { }) => {
 
   return createPlugin({
     setConfig: (config) => {
+      // TODO: since we're only given an entrypoint .storybook/config.js with a configure
+      // call to load stories on the client, we won't know which mdx files to create so
+      // we may have to inject entries dynamically on the client as part of the 'configure'
+      // process. after configure, we add all story kinds to the client-side entries?
+
+      // TODO: docz will only transpile files from src/* so our temp mdx needs to exist
+      // somewhere in the source tree (not temp), possibly in .docz/storybook
       const testMDX = path.join(paths.temp, 'test.mdx')
       fs.writeFileSync(testMDX, `
 import '${paths.storybook.config}'
