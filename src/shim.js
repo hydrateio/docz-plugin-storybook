76b6ad7946bf159cba2aa5c7cdc2637adf86f0cd
Travis Fischer <fisch0920@gmail.com>
2018-10-24 22:43:08 -0400

    .

--- src/shim.js
+++ src/shim.js
@@ -10,8 +10,13 @@ export const addParameters = clientAPI.addParameters
 export const clearDecorators = clientAPI.clearDecorators
 export const getStorybook = clientAPI.getStorybook
 
-// these deal with hot reloading and ensuring that story state and the react i
-// dom are properly updated which isn't really necessary for our use case, so
-// we export empty stubs for the sake of compatibility.
-export const configure = () => { } // TODO: noop
 export const forceReRender = () => { } // TODO: noop
+
+export const configure = (loaders, module) => {
+  if (loaders) {
+    // load all stories into the client API via one or more imperative commonjs
+    // 'require' trees
+    // TODO: is this setTimeout necessary?
+    setTimeout(() => loaders())
+  }
+}
