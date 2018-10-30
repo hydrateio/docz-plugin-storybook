460d3eee672d087a42136383364447e4bfd5861d
Travis Fischer <fisch0920@gmail.com>
2018-10-30 13:09:23 -0400

    support automated storybook population via MAGIC

--- examples/basic/storybook/button-story.js
+++ examples/basic/storybook/button-story.js
@@ -3,7 +3,9 @@ import { storiesOf } from '@storybook/react'
 import Button from '../components/Button'
 
 storiesOf('Button', module)
-  .add('with text', () => <Button>Hello Button</Button>)
+  .add('with text', () => (
+    <Button>Hello Button</Button>
+  ))
   .add('with some emoji', () => (
     <Button>
       <span role={'img'} aria-label={'so cool'}>
