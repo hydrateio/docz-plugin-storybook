9f34dd37974ce23a558e00385b8f0f4f9491cc40
Kevin Davis <kevin@hydrate.io>
2018-11-07 22:03:23 -0600

    add examples showing how to wrap with styled-frame and styled-shadow-dom

--- examples/basic/components/Button.jsx
+++ examples/basic/components/Button.jsx
@@ -1,7 +1,13 @@
 import React from 'react'
+import styled from 'styled-components'
 
-const Button = ({ onClick, children }) => (
-  <button onClick={onClick}>{children}</button>
-)
+const Button = ({ onClick, children }) => <button onClick={onClick}>{children}</button>
+
+const StyledButton = styled('button')`
+  appearance: none;
+  background: purple;
+  color: white;
+`
 
 export default Button
+export { StyledButton }
