8764090e1519390694e87015230068f642dabb57
Travis Fischer <fisch0920@gmail.com>
2018-11-08 22:05:51 -0500

    feat: add support for static asset directory

--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
 import { createPlugin } from 'docz-core'
 import * as path from 'path'
 import fs from 'fs-extra'
+import koaStatic from 'koa-static'
+import slugify from '@sindresorhus/slugify'
 import paths from './paths'
 import buildStorybook from './build-storybook'
 
@@ -8,8 +10,10 @@ export const storybook = (opts = { }) => {
   const {
     // these options are intended to mimic the storybook cli options
     configDir = paths.storybook.configDir,
+    staticDir,
     storyWrapper = './default-wrapper',
-    autofill = true
+    autofill = false,
+    stories
   } = opts
 
   const storybookConfigPath = path.resolve(configDir, paths.storybook.config)
@@ -17,14 +21,19 @@ export const storybook = (opts = { }) => {
 
   return createPlugin({
     setConfig: async (config) => {
-      if (autofill) {
-        const storybook = await buildStorybook({ configDir })
-        console.log('storybook', JSON.stringify(storybook, null, 2))
+      const storybook = autofill
+        ? await buildStorybook({ configDir })
+        : stories
+
+      if (storybook && storybook.length) {
+        // console.log('storybook', JSON.stringify(storybook, null, 2))
 
         await fs.ensureDir(paths.temp)
         await Promise.all(storybook.map((storyKind) => {
           const { kind, stories } = storyKind
+          const kindSlug = slugify(kind)
 
+          // TODO: support " characters in kind and name
           const content = `
 ---
 name: ${kind} Stories
@@ -37,11 +46,11 @@ import { Story } from 'docz-plugin-storybook/dist/react'
 ${stories.map(({ name }) => `
 ## ${name}
 
-<Story kind='${kind}' name='${name}' />
+<Story kind="${kind}" name="${name}" />
 
-`)}
+`).join('\n\n')}
 `
-          const file = path.join(paths.temp, `${kind}-storybook.mdx`)
+          const file = path.join(paths.temp, `${kindSlug}-storybook.mdx`)
           storybookFiles.push(file)
           return fs.writeFile(file, content)
         }))
@@ -70,8 +79,6 @@ ${stories.map(({ name }) => `
       config.resolve = config.resolve || {}
       config.resolve.alias = config.resolve.alias || {}
       config.resolve.alias['@storybook/react'] = path.resolve(__dirname, './shim')
-
-      // create a wrapper around each component for isolation and surface it as docz-plugin-storybook/story-wrapper
       config.resolve.alias['docz-plugin-storybook/story-wrapper'] = require.resolve(storyWrapper)
 
       if (fs.existsSync(webpackConfigPath)) {
@@ -91,6 +98,21 @@ ${stories.map(({ name }) => `
       */
 
       return config
+    },
+
+    onCreateApp: (app) => {
+      console.log('onCreateApp', app)
+
+      if (staticDir) {
+        const staticPath = path.resolve(staticDir)
+
+        if (!fs.existsSync(staticPath)) {
+          console.error(`Error: no such directory to load static files: ${staticPath}`)
+          process.exit(-1)
+        }
+
+        app.use(koaStatic(staticPath))
+      }
     }
   })
 }
