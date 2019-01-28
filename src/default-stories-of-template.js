b71eca80a364760970ae1c9d3af85721d71a8986
Joel Peterson <joelpeterson@Joels-MacBook-Pro-15.local>
2019-01-28 14:31:24 -0600

    added ability to generate playgrounds for components from the story definitions

--- src/default-stories-of-template.js
+++ src/default-stories-of-template.js
@@ -1,4 +1,39 @@
-export default ({ kind, stories }) => {
+const renderComponentPlayground = (component) => {
+  if (!component) return ''
+
+  const { children, ...defaultPlaygroundProps } = component.defaultPlaygroundProps || {}
+
+  const propString = Object.keys(defaultPlaygroundProps).map(key => {
+    return `${key}={${defaultPlaygroundProps[key]}}`
+  }).join(' ')
+
+  if (children) {
+    return `<Playground>
+  <${component.importName} ${propString}>
+    ${children}
+  </${component.importName}>
+</Playground>
+`
+  }
+
+  return `<Playground>
+  <${component.importName} ${propString} />
+</Playground>
+`
+}
+
+const renderComponentImports = (component) => {
+  if (!component) return ''
+
+  const imports = [
+    { importName: component.importName, importPath: component.importPath },
+    ...component.otherImports
+  ]
+
+  return imports.map(imp => `import ${imp.importName} from '${imp.importPath}'`).join('\n')
+}
+
+export default ({ kind, stories, component, defaultPlaygroundProps }) => {
   const routes = kind.split('/').map((p) => p.trim())
 
   let kindTitle = kind
@@ -16,8 +51,12 @@ ${menu ? `menu: ${menu}` : ''}
 ---
 
 import { Story } from 'docz-plugin-storybook/dist/react'
+import { Playground } from 'docz'
+${renderComponentImports(component)}
 
 # ${kind}
+
+${renderComponentPlayground(component, defaultPlaygroundProps)}
 
 ${stories.map(({ name }) => `
 ## ${name}
